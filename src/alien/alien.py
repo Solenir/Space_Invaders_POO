6929ed8ece617e6b7ed6a000013d5fd2edfdfc7f
Solenir <solenir.figueredo@gmail.com>
2025-03-23 18:52:02 -0300

    feat: adicionada classe que representa o grupo de alienígenas do jogo

--- src/alien/alien.py
+++ src/alien/alien.py
@@ -12,40 +12,40 @@ class Alien(sprite.Sprite):
         self.row = row
         self.column = column
         self.images = []  # Lista para armazenar as imagens do alienígena
-        self.load_images()  # Carrega as imagens para animação
+        self._load_images()  # Carrega as imagens para animação
         self.index = 0 # Índice da imagem atual
         self.image = self.images[self.index] # Define a imagem inicial
         self.rect = self.image.get_rect() # Referência ao objeto do jogo
         self.game = game
 
-    '''
-        Alterna entre as imagens do alienígena para criar um efeito de animação.
-    '''
-    def toggle_image(self):
+    
+    def _toggle_image(self):
+        """Alterna entre as imagens do alienígena para criar um efeito de animação."""
         self.index += 1
         if self.index >= len(self.images):
             self.index = 0 # Retorna ao início se ultrapassar o limite
         self.image = self.images[self.index]  # Atualiza a imagem
-
-    '''
-        Atualiza a posição e exibe a imagem na tela do jogo.        
-        Essa função é chamada a cada frame para redesenhar o alienígena.
-    '''
+   
     def update(self, *args):
+        """
+        Função responsável por atualizar a posição e exibe a imagem na tela do jogo.        
+        Essa função é chamada a cada frame para redesenhar o alienígena.
+        """
         self.game.screen.blit(self.image, self.rect)
 
-    '''
+    
+    def _load_images(self):
+        """
         Função responsável por carregar as imagens dos alienígenas com base na linha da formação.
         Cada alienígena tem duas imagens alternadas para criar um efeito de movimento.
-    '''
-    def load_images(self):
+        """
         images = {0: ['1_2', '1_1'],
                   1: ['2_2', '2_1'],
                   2: ['2_2', '2_1']
                   }
         # Obtém as imagens correspondentes à linha do alienígena
         img1, img2 = [constants.IMAGES[f'alien{img_num}'] for img_num in images[self.row]]
-                
+
         # Redimensiona e armazena as imagens
         self.images.append(transform.scale(img1, (40, 35)))
         self.images.append(transform.scale(img2, (40, 35)))
